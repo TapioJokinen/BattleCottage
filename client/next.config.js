1ef4800dd57a5209962a728b22d8b66757b552ed
Tapio Jokinen <37291216+TapioJokinen@users.noreply.github.com>
2023-10-09 18:26:43 +0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'battlecottage.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
