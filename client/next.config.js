5df06d58f2e5794b44e5c282b88f9481a51376be
Tapio Jokinen <37291216+TapioJokinen@users.noreply.github.com>
2023-10-13 12:25:10 +0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -6,20 +6,20 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-            protocol: 'https',
-            hostname: 'cdn.battlecottage.com',
-            port: '',
-            pathname: '/images/**',
+                protocol: 'https',
+                hostname: 'cdn.battlecottage.com',
+                port: '',
+                pathname: '/images/**',
             },
         ],
-        },
+    },
     async headers() {
         return [
             {
-                source: "/:path*",
+                source: "/(.*)",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "cdn.battlecottage.com" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
