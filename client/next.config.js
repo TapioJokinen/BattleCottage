27c0dc9341c84df5d934c0193c436282402d8a95
Tapio Jokinen <37291216+TapioJokinen@users.noreply.github.com>
2023-10-13 10:26:00 +0300

    yes

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+    output: 'standalone',
+    assetPrefix: isProd ? 'https://cdn.battlecottage.com' : undefined,
     images: {
-        domains: ['https://battlecottage.com'],
-    },
+        remotePatterns: [
+            {
+            protocol: 'https',
+            hostname: 'cdn.battlecottage.com',
+            port: '',
+            pathname: '/images/**',
+            },
+        ],
+        },
 };
 
 module.exports = nextConfig;
